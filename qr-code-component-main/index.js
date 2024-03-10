595342fe34e6bb4db18a7fbd7fcdcf0e92cd9d8b
Jc9697 <124229632+Jc9697@users.noreply.github.com>
2024-03-10 17:58:58 -0500

    modified:   qr-code-component-main/index.js

--- qr-code-component-main/index.js
+++ qr-code-component-main/index.js
@@ -3,7 +3,7 @@ const image = document.getElementById('image');
 image.addEventListener('click', () => {
     console.log('clicked')
 })
-
+/* Just testing touchstart */
 image.addEventListener('touchstart', () => {
     document.querySelector('body').style.backgroundColor = 'red'
 })
